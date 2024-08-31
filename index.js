1569181f6c5255ffb9ba95e5c1face8fcc5a9958
Anuj Pandey <120494847+AnujPandey123@users.noreply.github.com>
2024-08-31 17:49:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const path = require("path"); // Required to resolve paths correctly
 const app = express();
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
+const router = express.Router();
 
 app.use(express.json());
 app.use(cors());
@@ -18,22 +18,15 @@ const verifyToken = require("./Middleware/middleware");
 const RecipeRoute = require("./routes/RecipeRoute");
 const ForgotPassword = require("./routes/forgotPassword");
 
-// Set up routes
 app.use("/auth", LoginRoute);
 app.use("/auth", RegisterRoute);
 app.use("/auth", RecipeRoute);
+app.use("/auth", router);
 app.use("/auth", ForgotPassword);
 
-// Set up your home route with middleware
-app.get("/", verifyToken, Home.Home);
+router.get("/", verifyToken, Home.Home);
 
-// Serve static files from the React app's build directory
-app.use(express.static(path.join("/", "client/build"))); // Adjust 'client/build' to your React app's build folder location
-
-// Catch-all route to handle any other route and serve the index.html of React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("/", "client/build", "index.html")); // Adjust path if needed
-});
+module.exports = router;
 
 if (config) {
   app.listen(process.env.PORT, () => {
